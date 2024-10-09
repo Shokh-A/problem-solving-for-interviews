17311a6329e3844573933b8cf668fa29e077d139
Shokh <20shoxjaxon02@gmail.com>
2024-10-09 22:36:51 +0200

    Template code created for problem solving

---
+++ template.py
@@ -0,0 +1,21 @@
+
+
+# Problem:
+# 
+
+# Brainstorming:
+#
+
+class Solution:
+
+  def func(self):
+    pass
+
+# Test
+sol = Solution()
+# Test case 1
+print("1st test case", sol.func())
+# Test case 2
+print("2nd test case", sol.func())
+# Test case 3
+print("3rd test case", sol.func())
